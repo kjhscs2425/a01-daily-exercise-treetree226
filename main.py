a94c137c2df4722ca5bb2d2d2fedbff7d88a54dc
treetree226 <tthai2025@kehillahstudent.org>
2024-09-11 12:08:10 -0700

    finished

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+from curses import window
 import turtle
+
+from turtle import right, left, forward, backward
+
+for i in range(6):
+    left(60)
+    forward(40)
+left(120)
+forward(80)
+backward(40)
+right(60)
+forward(40)
+backward(80)
+forward(40)
+right(60)
+forward(40)
+backward(80)
+
+turtle.exitonclick()
+
+
 
